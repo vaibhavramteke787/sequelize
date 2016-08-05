684c4c97fe55d05b919c5e2db579b617b8f0b083
vaibhavramteke787 <vaibhavramteke787@users.noreply.github.com>
2016-08-05 18:31:35 +0530

    Updated sscce.js
    the models are in individual files , which are loaded by index.js through  var models = require('./models/index');

--- sscce_template.js
+++ sscce_template.js
@@ -6,3 +6,128 @@
 
 var Sequelize = require('./index');
 var sequelize = require('./test/support').createSequelizeInstance();
+
+
+ var Students = sequelize.define('Students', {
+        Id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'Students',
+                key: 'Id'
+            }
+        },
+        Name: {
+            type: 'NCHAR',
+            allowNull: true
+        }
+    },
+        { timestamps: false },
+        { tableName: 'Students' },
+        {
+            classMethods: {
+                associate: function (models) {
+                    Students.belongsToMany(models.Depts,
+                        { foreignKey: 'StudentId', through: models.StudentDepts, unique: false });
+                }
+            }
+        }
+    );
+    
+    
+      var Depts = sequelize.define('Depts', {
+        Id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true
+        },
+        Name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+        {
+            classMethods: {
+                associate: function (models) {
+
+                    Depts.hasMany(models.Employees, {
+                        foreignKey: 'DeptId'
+                    });
+                    Depts.belongsToMany(models.Students, {
+                        foreignKey: 'DeptId',
+                        through: models.StudentDepts,
+                        unique: false
+                    });
+                }
+
+            },
+            timestamps: false
+        },
+        {
+            tableName: 'Depts'
+        }
+
+    );
+    
+    var StudentDepts =  sequelize.define('StudentDepts', {
+		StudentId: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			references: {
+				model: 'Students',
+				key: 'Id'
+			}
+		},
+		DeptId: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			references: {
+				model: 'Depts',
+				key: 'Id'
+			}
+		}
+    },
+        { timestamps: false },
+        {
+		tableName: 'StudentDepts'
+        });
+        
+          var Employees = sequelize.define('Employees', {
+        Id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true
+        },
+        Name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        DeptId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        }
+    },
+        {
+            classMethods: {
+                associate: function (models) {
+                    Employees.belongsTo(models.Depts);
+                }
+            },
+            timestamps: false
+        },
+
+        {
+            tableName: 'Employees'
+        }
+    );
+    
+    
+    Depts.findAll({ raw: true, include: models.Students }).then(function (data) {
+    console.log(data);
+   
+});
+
+Students.findAll({ raw: true, include: models.Depts }).then(function (data) {
+    console.log(data);
+   
+});
